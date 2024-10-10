fcd52406a483e3487ec91540c69b09e1faa9faf9
Hafizh Ibnu Syam <aqrashu.asus@gmail.com>
2024-10-10 16:50:11 +0700

    Refactor file upload to use temporary files more efficiently
    Updated the file upload logic to create temporary files that are deleted automatically after use, simplifying the cleanup process. This improves resource management and reduces the risk of leftover files lingering in local storage. Additionally, adjusted the calling method in cover letter generation to eliminate unnecessary file path handling, streamlining the overall code.

--- coverletter/views.py
+++ coverletter/views.py
@@ -36,17 +36,16 @@ def get_file_extension(file_to_upload):
 
 def _upload_file(file_to_upload):
     suffix = get_file_extension(file_to_upload)
-    with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as temp_file:
+    with tempfile.NamedTemporaryFile(delete=True, suffix=suffix) as temp_file:
         temp_file.write(file_to_upload.read())
-        temp_file_path = temp_file.name
-    uploaded_file = genai.upload_file(temp_file_path)
+        uploaded_file = genai.upload_file(temp_file.name)
 
-    return uploaded_file, temp_file_path
+    return uploaded_file
 
 def generate_cover_letter(resume: bytes, open_position: bytes, language: str) -> str:
     try:
-        pdf_resume, pdf_resume_path = _upload_file(resume)
-        open_position, open_position_path = _upload_file(open_position)
+        pdf_resume = _upload_file(resume)
+        open_position = _upload_file(open_position)
 
         prompt = f'''
 Imagine you're scrolling through Instagram and come across an intriguing job posting that aligns perfectly with your career aspirations. Craft a professional email to the HR department expressing your enthusiasm and detailing how your skills, as outlined in your resume, make you an ideal candidate for this role. Consider how you can articulate your unique strengths without referring to details that aren't explicitly provided in the job listing and resume. Also, make sure your language in {language} to mirrors the tone and language of the job listing. How will you ensure that your message stands out among potential applicants? Don't explain your response.
@@ -54,9 +53,7 @@ Imagine you're scrolling through Instagram and come across an intriguing job pos
 
         response = model.generate_content([prompt, pdf_resume, open_position])
 
-        # delete from local storage and cloud
-        os.remove(pdf_resume_path)
-        os.remove(open_position_path)
+        # Delete files from the cloud, as they have already been removed locally after the upload
         pdf_resume.delete()
         open_position.delete()
 
